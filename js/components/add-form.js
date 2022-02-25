f3969c207d9abe4ad723a2fc59c268dfa1d1d334
david <dsolanoreyes@my.wctc.edu>
2022-02-25 00:43:54 -0600

    added more books. buttons still don't work

--- js/components/add-form.js
+++ js/components/add-form.js
@@ -9,7 +9,7 @@ Vue.component('products', {
     template: `
     <b-container class="p-0 m-0">
         <b-row fluid="lg" v-for="book in booklist" class="justify-content-md-center">
-            <book class="" v-bind:book="book" v-bind:booklist="booklist"></book> 
+            <book v-bind:book="book" v-bind:booklist="booklist" @add-to-cart="updateCart"></book> 
         </b-row>
     </b-container>
     
@@ -46,12 +46,50 @@ Vue.component('book', {
         booklist: {
             type: Array,
             required: true
-        }
+        },
+
+
     },
 
     template: `
+<b-col cols="6" style="padding: 5px">
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+        <b-row no-gutters>
+            <b-col md="4">
+                <b-card-img :src="book.image" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col md="8">
+                <b-card-body :title="book.product">
+                    <div v-for="stuff in book.details">
+                        <b-card-text>
+                            {{stuff}}
+                        </b-card-text>
+                    </div>
+                    <b-card-text>
+                        <b-alert v-model="book.getProductDetails" variant="dark" dismissible>
+                            <h4>Book Information</h4>
+                            <hr>
+                            <p>
+                                {{ book.bookDetails }}
+                            </p>
+                        </b-alert>
+                    </b-card-text>
+                    <hr>
+                    <!--add to cart button-->
+                    <b-button v-on:click="book.addToCart"
+                              :disabled="!book.inStock"
+                              :class="{ disabledButton: !book.inStock }" variant="primary">Add to Cart</b-button>
 
-<b-col cols="4">
+                    <!--info button-->
+                    <b-button @click="book.getProductDetails=true" variant="primary">Get Info</b-button>
+                    
+                </b-card-body>
+            </b-col>
+        </b-row>
+    </b-card>
+</b-col>
+
+<!--<b-col cols="4">
     <b-card
     :title="book.product"
     
@@ -76,13 +114,14 @@ Vue.component('book', {
         </p>
     </b-alert>
     
-    <!--add to cart button-->
+    &lt;!&ndash;add to cart button&ndash;&gt;
     <b-button v-on:click="book.addToCart"
      :disabled="!book.inStock"
       :class="{ disabledButton: !book.inStock }" variant="primary">Add to Cart</b-button>
-      <!--info button-->
+      
+      &lt;!&ndash;info button&ndash;&gt;
     <b-button @click="book.getProductDetails=true" variant="primary">Get Info</b-button>
   </b-card>
-</b-col>
+</b-col>-->
     `
 })
